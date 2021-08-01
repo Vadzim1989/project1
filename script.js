91c0bfe106dcaf48210c5e58f87892ecec7009f9
Web <web@it.step>
2021-08-01 21:05:07 +0300

    some changings in script

--- script.js
+++ script.js
@@ -37,11 +37,12 @@ const listElement = [...list.children]; // create array with elements
 
 function createElem (content) { // making element 
     const elem = document.createElement('li');
+    elem.href = 'https://google.com';
     elem.textContent = content;
     return elem;
 }
 const crElem = createElem('item FROM JS'); // elem with content
-const crElemCopy = crElem.cloneNode(true);
+const crElemCopy = crElem.cloneNode(true); // deep clone (without TRUE, copy just an element)
 // list.append(crElem); // add element to UL in HTML (add elem to end)
 // list.prepend(crElem); // add elemnt to start
 
@@ -59,5 +60,89 @@ const crElemCopy = crElem.cloneNode(true);
 // list.insertAdjacentText('beforeend', '<li>INSERTED VIA ADJ</li>');
 // list.insertAdjacentElement('beforeend', crElem);
 
-list.append(crElem);
-list.prepend(crElemCopy);
+// list.append(crElem);
+// list.prepend(crElemCopy);
+
+
+// const card = document.getElementById('card');
+// const a = createElem('A FROM JS');
+
+// const btn1 = card.getElementsByTagName('button')[0]; //get 
+// const btn2 = card.children[card.children.length - 1];
+// const btn3 = card.querySelector('button');
+
+// card.insertBefore(a, btn1);
+
+// WORKS WITH EVENTS --------------------------------------------------
+
+// console.dir(list);
+
+// list.onclick = function() {
+//     console.log('click happened');
+// }
+
+// list.onmouseenter = function() {
+//     console.log('mouse entered');
+// }
+
+const btn = document.getElementById('btn');
+
+const btnClick = () => {
+    listElement[listElement.length - 1].remove();
+    listElement.pop();
+}
+
+btn.addEventListener('click', btnClick);
+
+// btn.ondblclick = () => {
+//     console.log('DBL click');
+// }
+
+// btn.oncontextmenu = function() {
+//     console.log('context menu');
+// }
+
+// list.onmousemove = () => {
+//     console.log('mouse move');
+// }
+
+// list.onmousedown = () => {
+//     console.log('mouse down');
+// }
+
+// list.onmouseup = () => {
+//     console.log('mouse up');
+// }
+
+// window.onresize = () => {
+//     console.log('window resize');
+// }
+
+
+
+list.addEventListener('click', () => {
+    console.log('click');
+});
+
+const listadd = () => {
+    console.log('click2');
+}
+
+list.addEventListener('click', listadd);
+
+list.removeEventListener('click', listadd);
+
+
+// -------------------------------------------------------------------
+
+// const clickHandler = function(){ // function
+//     console.log(this.textContent);
+// }
+
+const clickHandler = (event) => { // ver.2 with () =>
+    console.log(event.target.textContent);
+}
+
+listElement.forEach((li) => {
+    li.addEventListener('click', clickHandler);
+})
